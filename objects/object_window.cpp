319b2b8e29c6ca9b736f323dcab54116e4eb60a7
Anders Söderholm <andso518@student.liu.se>
2013-09-08 14:43:10 +0200

    object_window added

--- objects/object_window.cpp
+++ objects/object_window.cpp
@@ -1,25 +1,100 @@
 #include "object_window.h"
 
-
-void window::makeButton(const std::string& text, const int& xPos, const int& yPos, const std::string command)
+namespace object
 {
-  objects.push_back(new button(text, xPos, yPos, command));
-} 
-
-void window::handleEvents(SDL_Event event, pointer_arrow arrow )
-{
+  void window::makeButton(const std::string& text, const int& xPos, const int& yPos, const std::string command)
+  {
+    button* tmp = new button(text, xPos, yPos, command);
+    objects.push_back(tmp);
+  } 
   
-  for(auto i: objects)
-    {
-      if(static_cast<active_object*>(*i) != nullptr)
-	{
-	  static_cast<active_object>(*i)->handleEvent(event, arrow);
-	}
-    }
+  window::~window()
+  {
+    clean_up( {background,topBorder,bottomBorder,leftBorder,rightBorder,bottomRightCorner,bottomLeftCorner,topRightCorner, topLeftCorner},{});
+    
+    for(auto i : objects)
+      {
+	delete i;
+      }
+  }  
+  void window::makeTextBox(/*konstruktor argument för en textbox*/)
+  {
+    
+    // textBox* tmp = new textBox();
+    //objects.push_back(tmp);
+    
+  }
   
-}
-
-void window::print(SDL_Surface* screen)
-{
+  void window::makeSlider()
+  {
+  }
+  void window::makeCheckBox()
+  {
+  }
+  std::string window::handle_event(const SDL_Event& event, const pointer_arrow& arrow )
+  {
+    std::string tmpCommand = "";
+    for(auto i: objects)
+      {
+	if(dynamic_cast<active_object*>(i) != nullptr)
+	  {
+	    tmpCommand =  dynamic_cast<active_object*>(i)->handle_event(event, arrow);
+	  }
+      }
+    
+    return tmpCommand;
+    
+  }
   
-}
+  
+  void window::print(SDL_Surface* screen)
+  {
+    //dessa SDL_Rects används för att se till att backgrunden och ramen "skalas om" till rätt storlek
+    SDL_Rect backgroundSize;
+    backgroundSize.x = 0;
+    backgroundSize.y = 0;
+    backgroundSize.w = box.w;
+    backgroundSize.h = box.h;
+    SDL_Rect borderLength;
+    borderLength.x = 0;
+    borderLength.y = 0;
+    borderLength.w = box.w -10;
+    borderLength.h = 5;
+    SDL_Rect borderHeight;
+    borderHeight.x = 0;
+    borderHeight.y = 0;
+    borderHeight.w = 5;
+    borderHeight.h = box.h - 10;
+    
+    
+    //ritar ut fönstrets backgrund och ram
+    apply_surface(box.x, box.y, background, screen, &backgroundSize);
+    apply_surface(box.x +5, box.y, topBorder, screen, &borderLength);
+    apply_surface(box.x +5, box.y+box.h - 5, bottomBorder, screen, &borderLength);
+    apply_surface(box.x+box.w - 5, box.y + box.h - 5, bottomRightCorner,screen);
+    apply_surface(box.x,box.y+box.h - 5, bottomLeftCorner, screen);
+    apply_surface(box.x, box.y, topLeftCorner, screen);
+    apply_surface(box.x+box.w -5, box.y, topRightCorner, screen);
+    apply_surface(box.x, box.y + 5, leftBorder, screen, &borderHeight);
+    apply_surface(box.x + box.w - 5, box.y + 5 , rightBorder, screen, &borderHeight);
+    
+    //ritar ut alla object som hör till detta window
+    for(auto i : objects)
+      {
+	i->print(screen);
+      }
+    
+  }
+  
+  
+  bool window::loaded()
+  {
+    
+    if(background != nullptr)
+      {
+	return true;
+      }
+    
+    return false;
+  }
+}//slut på namnrymd
--- objects/object_window.h
+++ objects/object_window.h
@@ -1,33 +1,56 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
-#include "functions.h"
-#include "button.h"
+#include "../functions.h"
+#include "object_button.h"
 #include "objects.h"
 #include <vector>
 
 
 namespace object
 {
-
-class window
-{
-private:
-SDL_Surface* background = nullptr;
-SDL_Surface* top_border = nullptr;
-SDL_Surface* left_border = nullptr;
-SDL_Surface* right_border = nullptr;
-SDL_Surface* bottom_border = nullptr;
-SDL_Rect box;
-
-vector<object*> objects;
-public:
- window(const int& x, const int& y, const int& w, const int& h,) : box.x(x),box.y(y),box.w(w),box.h(h)
+  class window : public active_object
   {
-    //ladda background och borders
-  }
- void handleEvents(SDL_Event event);
- void makeButton(const std::string& text, const int& xPos, const int& yPos, const std::string command = "");
- void print(SDL_Surface* screen);
-};
-}//namespace
+  private:
+    SDL_Surface* background = nullptr;
+    SDL_Surface* topBorder = nullptr;
+    SDL_Surface* leftBorder = nullptr;
+    SDL_Surface* rightBorder = nullptr;
+    SDL_Surface* bottomBorder = nullptr;
+    SDL_Surface* bottomRightCorner = nullptr;
+    SDL_Surface* bottomLeftCorner = nullptr;
+    SDL_Surface* topLeftCorner = nullptr;
+    SDL_Surface* topRightCorner = nullptr;
+    SDL_Rect box;
+    
+    std::vector<object*> objects;
+  public:
+    //notera att  fönstret bör vara minst 10x10 pixlar stort
+    window(const int& x, const int& y, const int& w, const int& h)
+      {
+	//ladda background och borders
+	box.x = x;
+	box.y = y;
+	box.w = w;
+	box.h = h;
+	background = load_image("Images/Gui/Window/background.png", false);
+	topBorder = load_image("Images/Gui/Window/tb.png", false);
+	bottomBorder = load_image("Images/Gui/Window/bb.png", false);
+	bottomRightCorner = load_image("Images/Gui/Window/rbc.png", false);
+	bottomLeftCorner = load_image("Images/Gui/Window/lbc.png", false);
+	topLeftCorner = load_image("Images/Gui/Window/ltc.png", false);
+	topRightCorner = load_image("Images/Gui/Window/rtc.png", false);
+	leftBorder = load_image("Images/Gui/Window/lb.png", false);
+	rightBorder = load_image("Images/Gui/Window/rb.png", false);
+      }
+    ~window();
+    std::string handle_event(const SDL_Event&, const pointer_arrow&);
+    void makeButton(const std::string& text, const int& xPos, const int& yPos, const std::string command = "");
+    void makeSlider(/*konstruktor argument för slider*/);
+    void makeCheckBox(/*konstruktor argument för checkBox*/);
+    void makeTextBox(/*konstruktor argument för textbox*/);
+    void print(SDL_Surface* screen);
+    bool loaded();
+  };  
+}//namnrymd
+#endif
