a577643b7dbc76ccc89336f5204f82c44461d828
Anders Söderholm <andso518@student.liu.se>
2013-11-18 16:24:25 +0100

    lite fixat

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CCC        =g++
 CFLAGS   +=-L/sw/gcc-${GCC4_V}/lib -static-libstdc++ -std=c++11 -pedantic -Wall -Wextra
 INCLUDE  = -I./objects
 SDLFLAGS +=-lmingw32 -lSDLmain -lSDL -lSDL_image -lSDL_ttf -lSDL_mixer
-OBJECTS = main.cpp gameCard.o heroCard.o card.o menu.o game.o Button.o Slider.o Textbox.o Checkbox.o Functions.o Window.o music.o SGS.o cardList.o
+OBJECTS = main.cpp gameCard.o heroCard.o card.o menu.o game.o Button.o Slider.o Textbox.o Checkbox.o Functions.o Window.o music.o SGS.o cardList.o Player.o
 all: $(OBJECTS)
 		@ echo
 		@ echo execute skapas
@@ -70,6 +70,9 @@ card.o: ./objects/card.h ./objects/card.cpp
 		@ echo card.o skapas
 		@$(CCC) $(CFLAGS) -c ./objects/card.cpp $(SDLFLAGS)
 		
+Player.o: Player.h Player.cpp
+			@ echo Player.o skapas
+			@ $(CCC) $(INCLUDE) $(CFLAGS) -c Player.cpp $(SDLFLAGS)
 		
 		
 clean:
--- Player.cpp
+++ Player.cpp
@@ -1,22 +1,127 @@
 #include "Player.h"
-/*
-	int current_life;
-	int max_life;
-	CardList hand;
-	Equipment equipment;
-	HeroCard hero;
-	stack<GameCard> judgement_cards;
-*/
 
-	Card* Player::playCard(int index)
-	void Player::receiveCard(Card* what_card)
-	Card* Player::loseCard(int index)
-	Card* Player::loseEquipment(int index)
-	void Player::setHero(HeroCard* character)
+
+using namespace Object;
+
+Player::Player()
+{
+  
+}
+
+Player::~Player()
+{
+}
+
+void Player::setHero(HeroCard* character)
+{
+  max_life = character->getHP();
+  current_life = character->getHP();
+  male = character->getMale();
+  hero = character;
+}
+
+GameCard* Player::playCard(int index)
+{
+  GameCard* played_card = hand.at(index);
+  std::vector<GameCard*>::iterator it = hand.begin() + index;
+  hand.erase(it);
+  
+  fixCardPosition();
+  return played_card;
+}
+
+GameCard* Player::loseCard(int index)
+{
+  GameCard* lost_card = hand.at(index);
+  std::vector<GameCard*>::iterator it = hand.begin() + index;
+  hand.erase(it);
+
+  fixCardPosition();
+  return lost_card;
+}
+
+void Player::recieveCard(GameCard* new_card)
+{
+  hand.push_back(new_card);
+  fixCardPosition();
+}
+
+GameCard* Player::loseEquipment(int index)
+{
+  GameCard* lost_equipment = eq.equipment.at(index);
+  
+  std::vector<GameCard*>::iterator it = eq.equipment.begin() + index;
+  eq.equipment.erase(it);
+
+  return lost_equipment;
+  
+}
+
+void Player::doEffect(std::string what_effect)
+{
+  //implementera senare
+
+}
+
+void Player::setStatus(unsigned status)
+{
+  game_status = status;
+}
+
+bool Player::entered()
+{
+  return game_status == 1;
+}
+
+bool Player::loading()
+{
+  return game_status == 0;
+}
+
+bool Player::left()
+{
+  return game_status == 2;
+}
+
+void Player::setPlayerNr(int nr)
+{
+  player_nr = nr;
+}
+
+void Player::paint(SDL_Surface* screen, bool current_player)
+{
+  //rita ut handen längst ner på skärmen
+  if(current_player == true)
+    {
+      for(auto i : hand)
 	{
-		current_life = character -> getHP();
-		max_life = current_life;
-		male = character -> getMale();
-		
-	void Player::doEffect(string what_effect)
-	void Player::paint()
+	  i->paint(screen);
+	}
+    }
+  
+  hero->paint(screen);
+
+  //rita ut equipment
+}
+
+void Player::paint(SDL_Surface* screen, int x_pos, int y_pos)
+{
+  
+  hero->setPosition(x_pos, y_pos);
+  hero->paint(screen);
+
+  //rita ut equipment
+
+}
+
+void Player::fixCardPosition()
+{
+  int x_pos = 50;
+  int y_pos = 500;
+  
+  for(auto i : hand)
+    {
+      i->setPosition(x_pos, y_pos);
+    }
+  hero->setPosition(550, 475);
+}
