5fd3da3302d944fd7abc0818e61b8bd520b44980
justiss <justus.sorman@gmail.com>
2013-11-19 15:33:05 +0100

    Framerate fixer ditlagd men ej implementerad

---
+++ FrameRateFixer.h
@@ -0,0 +1,20 @@
+
+//The timer
+class FrameRateFixer
+{
+private:
+	unsigned FPS;
+	unsigned frameDelay;
+    unsigned startTicks;
+	unsigned lastTick;
+	
+    unsigned get_ticks();
+public:
+    FrameRateFixer(unsigned fps = 30);
+	~FrameRateFixer() = default;
+	
+	void start();
+	void regulateFPS();
+	void setFPS(unsigned);
+	unsigned getFrameTime();
+};
